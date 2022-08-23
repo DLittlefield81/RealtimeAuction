86c431fdf9eb4804f1eccfd6308c97082095a443
Dennis <littlefield.dennis@outlook.com>
2022-08-23 00:17:46 -0400

    functioning app, now to style

--- client/src/components/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from "./Home";
-import AddProduct from "./AddProduct";
-import BidProduct from "./BidProduct";
-import Products from "./Products";
-import Nav from "./Nav";
-
-export { Home, AddProduct, BidProduct, Products, Nav };
